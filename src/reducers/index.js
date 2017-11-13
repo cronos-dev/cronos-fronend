cabc1d5b8570d8ff33848b9e60e6d4b2c94aa9e9
dio <abrahamdio@gmail.com>
2017-11-13 00:54:02 -0800

    add menu nav, cleanup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import GitReducer from './gitReducer.js';
 import ActiveRepo from './activeReducer.js';
+import MenuReducer from './menuReducer.js';
 const allReducers= combineReducers({
   repos: GitReducer,
+  menus: MenuReducer,
   activeRepo: ActiveRepo
 });
 export default allReducers;
